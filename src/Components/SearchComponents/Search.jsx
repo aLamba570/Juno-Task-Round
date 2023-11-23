bd57abe2dd48879ae0463b3695dbd79d31065043
Ankush <alamba570@gmail.com>
2023-11-23 20:34:24 +0530

    adds the search.jsx and search.css

--- src/Components/SearchComponents/Search.jsx
+++ src/Components/SearchComponents/Search.jsx
@@ -1,4 +1,5 @@
 import React, {useState} from "react";
+import "./search.css";
 
 const Search = () => {
     const [searchTerm, setSearchTerm] = useState("");
@@ -21,6 +22,30 @@ const Search = () => {
         <div className="container">
             <div className="search-container">
                 <input type="text" placeholder="search....." value={searchTerm} onChange={handleSearchChange} />
+            </div>
+
+            <div className="spinner-container">
+                <select id="triggerReason"
+                        value={triggerReason}
+                        onChange={handleTriggerReasonChange}
+                >
+                    <option value="">Select Trigger Reason</option>
+                    <option value="hardFlag">Hard Flag</option>
+                    <option value="tempFlag">Temp Flag</option>
+                    <option value="rUnFlag">Restricted UnFlag</option>
+                    <option value="unFlag">UnFlag</option>
+                    <option value="reviewed">Reviewed</option>   
+                </select>
+                <select
+                id="riskLevel"
+                value={riskLevel}
+                onChange={handleRiskLevelChange}
+                >
+                <option value="">Select Risk Level</option>
+                <option value="low">Low</option>
+                <option value="medium">Medium</option>
+                <option value="high">High</option>
+                </select>
             </div>
         </div>
     );
